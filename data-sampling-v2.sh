7429100a02e58b17e1eb7a81d5f092cc4f6b0784
sudipX0 <sudippoudel@sd.taylors.edu.my>
2025-10-15 22:35:04 +0545

    Model training colab ma hudai xa; aaja lai aankha dukhyo

--- data-sampling-v2.sh
+++ data-sampling-v2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 RAW_DATA=~/DF-SCAN/data/raw/ff-c23/FaceForensics++_C23
-SAMPLE_DATA=~/DF-SCAN/data/experiment_100_batch2/ff-c23/FaceForensics++_C23
+SAMPLE_DATA=~/DF-SCAN/data/experiment_100_batch10/ff-c23/FaceForensics++_C23
 
 mkdir -p $SAMPLE_DATA
 
@@ -11,7 +11,7 @@ for class in DeepFakeDetection Deepfakes Face2Face FaceShifter FaceSwap NeuralTe
     mkdir -p "$SAMPLE_DATA/$class"
 
     # Skip first 100 and take next 100
-    find "$RAW_DATA/$class" -maxdepth 1 -type f -name "*.mp4" | sort | tail -n +101 | head -n 100 | \
+    find "$RAW_DATA/$class" -maxdepth 1 -type f -name "*.mp4" | sort | tail -n +901 | head -n 100 | \
     while read file; do
         cp "$file" "$SAMPLE_DATA/$class/"
     done
